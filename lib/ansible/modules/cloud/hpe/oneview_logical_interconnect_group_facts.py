96bd4335a6b8e3985f083b5f076a177984e1b10c
Camila Balestrin <camila.balestrin@hpe.com>
2017-03-21 16:35:40 -0300

    Add module oneview_logical_interconnect_group_facts

---
+++ lib/ansible/modules/cloud/hpe/oneview_logical_interconnect_group_facts.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+
+ANSIBLE_METADATA = {'status': ['stableinterface'],
+                    'supported_by': 'committer',
+                    'version': '1.0'}
+
+DOCUMENTATION = '''
+---
+module: oneview_logical_interconnect_group_facts
+short_description: Retrieve facts about one or more of the OneView Logical Interconnect Groups.
+description:
+    - Retrieve facts about one or more of the Logical Interconnect Groups from OneView.
+version_added: "2.3"
+requirements:
+    - "python >= 2.7.9"
+    - "hpOneView >= 2.0.1"
+author: "Camila Balestrin (@balestrinc)"
+options:
+    name:
+      description:
+        - Logical Interconnect Group name.
+      required: false
+extends_documentation_fragment:
+    - oneview
+    - oneview.factsparams
+'''
+
+EXAMPLES = '''
+- name: Gather facts about all Logical Interconnect Groups
+  oneview_logical_interconnect_group_facts:
+    config: "{{ config_file_path }}"
+
+- debug: var=logical_interconnect_groups
+
+- name: Gather paginated, filtered and sorted facts about Logical Interconnect Groups
+  oneview_logical_interconnect_group_facts:
+    config: "{{ config }}"
+    params:
+      start: 0
+      count: 3
+      sort: 'name:descending'
+      filter: 'name=LIGName'
+
+- debug: var=logical_interconnect_groups
+
+- name: Gather facts about a Logical Interconnect Group by name
+  oneview_logical_interconnect_group_facts:
+    config: "{{ config_file_path }}"
+    name: logical lnterconnect group name
+
+- debug: var=logical_interconnect_groups
+'''
+
+RETURN = '''
+logical_interconnect_groups:
+    description: Has all the OneView facts about the Logical Interconnect Groups.
+    returned: Always, but can be null.
+    type: complex
+'''
+
+from ansible.module_utils.oneview import OneViewModuleBase
+
+
+class LogicalInterconnectGroupFactsModule(OneViewModuleBase):
+    def __init__(self):
+
+        argument_spec = dict(
+            name=dict(required=False, type='str'),
+            params=dict(required=False, type='dict'),
+        )
+
+        super(LogicalInterconnectGroupFactsModule, self).__init__(additional_arg_spec=argument_spec)
+
+    def execute_module(self):
+        if self.module.params.get('name'):
+            ligs = self.oneview_client.logical_interconnect_groups.get_by('name', self.module.params['name'])
+        else:
+            ligs = self.oneview_client.logical_interconnect_groups.get_all(**self.params)
+
+        return dict(changed=False, ansible_facts=dict(logical_interconnect_groups=ligs))
+
+
+def main():
+    LogicalInterconnectGroupFactsModule().run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/units/modules/cloud/hpe/test_oneview_logical_interconnect_group_facts.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+
+from hpe_test_utils import FactsParamsTestCase
+
+from ansible.modules.cloud.hpe.oneview_logical_interconnect_group_facts import LogicalInterconnectGroupFactsModule
+
+ERROR_MSG = 'Fake message error'
+
+PARAMS_GET_ALL = dict(
+    config='config.json',
+    name=None
+)
+
+PARAMS_GET_BY_NAME = dict(
+    config='config.json',
+    name="Test Logical Interconnect Group"
+)
+
+PRESENT_LIGS = [{
+    "name": "Test Logical Interconnect Group",
+    "uri": "/rest/logical-interconnect-groups/ebb4ada8-08df-400e-8fac-9ff987ac5140"
+}]
+
+
+class LogicalInterconnectGroupFactsSpec(unittest.TestCase, FactsParamsTestCase):
+    def setUp(self):
+        self.configure_mocks(self, LogicalInterconnectGroupFactsModule)
+        self.logical_interconnect_groups = self.mock_ov_client.logical_interconnect_groups
+        FactsParamsTestCase.configure_client_mock(self, self.logical_interconnect_groups)
+
+
+    def test_should_get_all_ligs(self):
+        self.logical_interconnect_groups.get_all.return_value = PRESENT_LIGS
+        self.mock_ansible_module.params = PARAMS_GET_ALL
+
+        LogicalInterconnectGroupFactsModule().run()
+
+        self.mock_ansible_module.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(logical_interconnect_groups=(PRESENT_LIGS))
+        )
+
+    def test_should_get_lig_by_name(self):
+        self.logical_interconnect_groups.get_by.return_value = PRESENT_LIGS
+        self.mock_ansible_module.params = PARAMS_GET_BY_NAME
+
+        LogicalInterconnectGroupFactsModule().run()
+
+        self.mock_ansible_module.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(logical_interconnect_groups=(PRESENT_LIGS))
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
