49f86e7866156483e5150eb252d1291e4ab59deb
Camila Balestrin <camila.balestrin@hpe.com>
2017-04-04 14:59:06 -0300

    oneview_fabric_facts

---
+++ lib/ansible/modules/cloud/hpe/oneview_fabric_facts.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+ANSIBLE_METADATA = {'status': ['stableinterface'],
+                     'supported_by': 'curated',
+                     'metadata_version': '1.0'}
+
+DOCUMENTATION = '''
+---
+module: oneview_fabric_facts
+short_description: Retrieve the facts about one or more of the OneView Fabrics.
+description:
+    - Retrieve the facts about one or more of the Fabrics from OneView.
+version_added: "2.3"
+requirements:
+    - "python >= 2.7.9"
+    - "hpOneView >= 2.0.1"
+author: "Camila Balestrin (@balestrinc)"
+options:
+    name:
+      description:
+        - Fabric name.
+      required: false
+    options:
+      description:
+            - "List with options to gather additional facts about an Fabrics and related resources.
+          Options allowed: C(reservedVlanRange)."
+      required: false
+extends_documentation_fragment:
+    - oneview
+    - oneview.factsparams
+'''
+
+EXAMPLES = '''
+- name: Gather facts about all Fabrics
+  oneview_fabric_facts:
+    config: "{{ config_file_path }}"
+
+- debug: var=fabrics
+
+- name: Gather paginated, filtered and sorted facts about Fabrics
+  oneview_fabric_facts:
+    config: "{{ config }}"
+    params:
+      start: 0
+      count: 3
+      sort: 'name:descending'
+      filter: 'name=DefaultFabric'
+
+- debug: var=fabrics
+
+- name: Gather facts about a Fabric by name
+  oneview_fabric_facts:
+    config: "{{ config_file_path }}"
+    name: DefaultFabric
+
+- debug: var=fabrics
+
+- name: Gather facts about a Fabric by name with options
+  oneview_fabric_facts:
+    config: "{{ config }}"
+    name: DefaultFabric
+    options:
+      - reservedVlanRange          # optional
+
+- debug: var=fabrics
+'''
+
+RETURN = '''
+fabrics:
+    description: Has all the OneView facts about the Fabrics.
+    returned: Always, but can be null.
+    type: complex
+fabric_reserved_vlan_range:
+    description: Has all the OneView facts about the reserved VLAN range
+    returned: When requested, but can be null.
+    type: complex
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.oneview import OneViewModuleBase
+
+
+class FabricFactsModule(OneViewModuleBase):
+    argument_spec = dict(
+        name=dict(required=False, type='str'),
+        options=dict(required=False, type='list'),
+        params=dict(required=False, type='dict')
+    )
+
+    def __init__(self):
+        super(FabricFactsModule, self).__init__(additional_arg_spec=self.argument_spec)
+        self.resource_client = self.oneview_client.fabrics
+
+    def execute_module(self):
+        ansible_facts = {}
+        name = self.module.params['name']
+        if name:
+            fabrics = self.oneview_client.fabrics.get_by('name', name)
+
+            if self.options and fabrics:
+                ansible_facts = self.__gather_optional_facts(fabrics[0])
+        else:
+            fabrics = self.oneview_client.fabrics.get_all(**self.params)
+
+        ansible_facts['fabrics'] = fabrics
+
+        return dict(changed=False, ansible_facts=dict(ansible_facts))
+
+    def __gather_optional_facts(self, fabric):
+        ansible_facts = {}
+
+        if self.options.get('reservedVlanRange'):
+            ansible_facts['fabric_reserved_vlan_range'] = self.resource_client.get_reserved_vlan_range(fabric['uri'])
+
+        return ansible_facts
+
+
+def main():
+    FabricFactsModule().run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/units/modules/cloud/hpe/test_oneview_fabric_facts.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from ansible.modules.cloud.hpe.oneview_fabric_facts import FabricFactsModule
+from hpe_test_utils import FactsParamsTestCase
+
+ERROR_MSG = 'Fake message error'
+
+PARAMS_GET_ALL = dict(
+    config='config.json',
+    name=None
+)
+
+PARAMS_GET_BY_NAME = dict(
+    config='config.json',
+    name="DefaultFabric"
+)
+
+PARAMS_GET_BY_NAME_WITH_OPTIONS = dict(
+    config='config.json',
+    name="DefaultFabric",
+    options=['reservedVlanRange']
+)
+
+PRESENT_FABRICS = [{
+    "name": "DefaultFabric",
+    "uri": "/rest/fabrics/421fe408-589a-4a7e-91c5-a998e1cf3ec1"
+}]
+
+PRESENT_FABRIC_VLAN_RANGE = [{
+    "name": "DefaultFabric",
+    "uri": "/rest/fabrics/421fe408-589a-4a7e-91c5-a998e1cf3ec1",
+    "reservedVlanRangeParameters": {
+        "start": 300,
+        "length": 62
+    }
+}]
+
+FABRIC_RESERVED_VLAN_RANGE = 'a7896ce7-c11d-4658-829d-142bc66a85e4'
+
+DEFAULT_FABRIC_VLAN_RANGE = dict(
+    name='New FC Network 2',
+    reservedVlanRangeParameters=dict(start=300, length=62)
+)
+
+
+class FabricFactsSpec(unittest.TestCase,
+                      FactsParamsTestCase):
+    def setUp(self):
+        self.configure_mocks(self, FabricFactsModule)
+        self.fabrics = self.mock_ov_client.fabrics
+        FactsParamsTestCase.configure_client_mock(self, self.fabrics)
+
+    def test_should_get_all(self):
+        self.fabrics.get_all.return_value = PRESENT_FABRICS
+        self.mock_ansible_module.params = PARAMS_GET_ALL
+
+        FabricFactsModule().run()
+
+        self.mock_ansible_module.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(fabrics=(PRESENT_FABRICS))
+        )
+
+    def test_should_get_by_name(self):
+        self.fabrics.get_by.return_value = PRESENT_FABRICS
+        self.mock_ansible_module.params = PARAMS_GET_BY_NAME
+
+        FabricFactsModule().run()
+
+        self.mock_ansible_module.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(fabrics=(PRESENT_FABRICS))
+        )
+
+    def test_should_get_fabric_by_name_with_options(self):
+        self.fabrics.get_by.return_value = PRESENT_FABRICS
+        self.fabrics.get_reserved_vlan_range.return_value = FABRIC_RESERVED_VLAN_RANGE
+        self.mock_ansible_module.params = PARAMS_GET_BY_NAME_WITH_OPTIONS
+
+        FabricFactsModule().run()
+
+        self.mock_ansible_module.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(fabrics=PRESENT_FABRICS,
+                               fabric_reserved_vlan_range=FABRIC_RESERVED_VLAN_RANGE)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
