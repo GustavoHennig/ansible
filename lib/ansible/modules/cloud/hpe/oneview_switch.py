5338dbc5f6daf444b63c4810280e877ecdb18f0b
Abilio Parada <abilio.parada@hpe.com>
2017-04-07 16:16:47 -0300

    Add support to Switches

---
+++ lib/ansible/modules/cloud/hpe/oneview_switch.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['stableinterface'],
+                    'supported_by': 'curated'}
+
+DOCUMENTATION = '''
+---
+module: oneview_switch
+short_description: Provides an interface to remove ToR Switch resources.
+description:
+    - Provides an interface to remove Top of Rack(ToR) Switch resources.
+      The switch resource will be removed if it is in an unmanaged state.
+      If the switch resource is associated with a Logical Switch, it's removal is treated as a hardware removal only.
+      A reference to the switch is mantained, and the resource is marked as 'Absent'.
+version_added: "2.3"
+requirements:
+    - "python >= 2.7.9"
+    - "hpOneView >= 3.0.0"
+author: "Bruno Souza (@bsouza)"
+options:
+    state:
+        description:
+            - Indicates the desired state for the Switch resource.
+              C(absent) will remove the resource from OneView, if it exists.
+              C(ports_updated) will update the switch ports.
+        choices: ['absent', 'ports_updated']
+    name:
+      description:
+        - Switch name.
+      required: true
+extends_documentation_fragment:
+    - oneview
+'''
+
+EXAMPLES = '''
+- name: Delete the Switch
+  oneview_switch:
+    config: "{{ config }}"
+    state: absent
+    name: "172.18.16.2"
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.oneview import OneViewModuleBase, HPOneViewValueError, HPOneViewResourceNotFound
+
+
+class SwitchModule(OneViewModuleBase):
+    MSG_DELETED = 'Switch deleted successfully.'
+    MSG_PORTS_UPDATED = "Switch ports updated successfully."
+    MSG_NOT_FOUND = 'Switch not found.'
+
+    def __init__(self):
+        argument_spec = dict(
+            state=dict(
+                required=True,
+                choices=['absent', 'ports_updated']
+            ),
+            data=dict(required=False, type='list'),
+            name=dict(required=True, type='str'),
+        )
+        super(SwitchModule, self).__init__(additional_arg_spec=argument_spec)
+
+        self.resource_client = self.oneview_client.switches
+
+    def execute_module(self):
+        resource = self.get_by_name(self.module.params.get('name'))
+
+        if self.state == 'absent':
+            return self.__delete(resource)
+
+        if not resource:
+            raise HPOneViewResourceNotFound(self.MSG_NOT_FOUND)
+        else:
+            return self.__update_ports(resource)
+
+    def __delete(self, resource):
+        if resource:
+            self.resource_client.delete(resource)
+            return dict(changed=True, msg=self.MSG_DELETED)
+        else:
+            return dict(changed=False, msg=self.MSG_NOT_FOUND)
+
+    def __update_ports(self, resource):
+        self.resource_client.update_ports(id_or_uri=resource["uri"], ports=self.data)
+        return dict(changed=True, msg=self.MSG_PORTS_UPDATED)
+
+
+def main():
+    SwitchModule().run()
+
+
+if __name__ == '__main__':
+    main()
